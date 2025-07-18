c37cccac0fa7e4d6d8f9a95d870eea635eab051c
tryinsmth <3mksuss1@gmail.com>
2025-07-18 20:25:44 +0300

    Update infinity_a21s.mk

--- infinity_a21s.mk
+++ infinity_a21s.mk
@@ -29,21 +29,6 @@ INFINITY_BUILD_TYPE := UNOFFICIAL
 # Maintainer Name
 INFINITY_MAINTAINER := Mustafa
 
-# Whether the package includes System BLURS
-TARGET_SUPPORTS_BLUR := true 
-
-# Whether the compiled package ships Google Apps:
-WITH_GAPPS := false
-
-# Whether the compiled package ships more (mostly unimportant) Google Apps:
-TARGET_SHIPS_FULL_GAPPS := false
-
-# Whether the compiled shipped gapps package uses Google Dialer:
-TARGET_SHIPS_GOOGLE_DIALER := false
-
-# Whether the compiled package ships Motorola Calculator:
-USE_MOTO_CALCULATOR := true
-
 ## Inherit some common infinity stuff
 $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
